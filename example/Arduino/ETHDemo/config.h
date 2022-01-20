80abe23de610c781e17e2325262dd43735b353e2
Michy <513673326@qq.com>
2022-01-20 13:49:42 +0800

    Update config.h

--- example/Arduino/ETHDemo/config.h
+++ example/Arduino/ETHDemo/config.h
@@ -12,7 +12,7 @@
    * ETH_CLOCK_GPIO16_OUT - 50MHz clock from internal APLL output on GPIO16 - possibly an inverter is needed for LAN8720
    * ETH_CLOCK_GPIO17_OUT - 50MHz clock from internal APLL inverted output on GPIO17 - tested with LAN8720
 */
- #define ETH_CLK_MODE    ETH_CLOCK_GPIO0_IN          
+ #define ETH_CLK_MODE    ETH_CLOCK_GPIO0_OUT          
 
 // Pin# of the enable signal for the external crystal oscillator (-1 to disable for internal APLL source)
 #define ETH_POWER_PIN   4
